1d6b6f9e53c931d29b3b075fdfc8fdf81ac85de6
steffkes <stefan@gelungen.es>
2026-06-08 16:29:43 +0200

    improve name & description
    used for title and open graph information

--- event.js
+++ event.js
@@ -2,8 +2,8 @@ export const event = {
   "@context": "https://schema.org",
   "@type": "SportsEvent",
   url: "https://eskalation.gelungen.es",
-  name: "Eskalation",
-  description: "Rise Against the Machine",
+  name: "Eskalation · Rise Against the Machine",
+  description: "In Bewegung war die Treppe bisher nie!",
   organizer: {
     "@type": "Person",
     name: "Stefan Matheis",
